0aba573231545bc0214778032d1b679e90b38567
Athanasios (Sakis) Chliopanos <athchliopanos@gmail.com>
2020-05-05 08:36:21 +0300

    Update stm32f446xx_spi_driver.h

--- myCode/STM32CubeIDE/stm32f4xx_drivers/drivers/inc/stm32f446xx_spi_driver.h
+++ myCode/STM32CubeIDE/stm32f4xx_drivers/drivers/inc/stm32f446xx_spi_driver.h
@@ -40,7 +40,7 @@ typedef struct {
 /*
  * SPI Application States
  */
-#define SPI_READY			0
+#define SPI_READY		0
 #define SPI_BUSY_IN_TX		1
 #define SPI_BUSY_IN_RX		2
 
@@ -61,16 +61,16 @@ typedef struct {
 /*
  * @SPI_BusConfig
  */
-#define SPI_BUS_CONFIG_FD					1
-#define SPI_BUS_CONFIG_HD					2
+#define SPI_BUS_CONFIG_FD			1
+#define SPI_BUS_CONFIG_HD			2
 #define SPI_BUS_CONFIG_SIMPLEX_RXONLY		3
 
 /*
  * @SPI_SclkSpeed
  */
-#define SPI_SCLK_SPEED_DIV2					0
-#define SPI_SCLK_SPEED_DIV4					1
-#define SPI_SCLK_SPEED_DIV8					2
+#define SPI_SCLK_SPEED_DIV2				0
+#define SPI_SCLK_SPEED_DIV4				1
+#define SPI_SCLK_SPEED_DIV8				2
 #define SPI_SCLK_SPEED_DIV16				3
 #define SPI_SCLK_SPEED_DIV32				4
 #define SPI_SCLK_SPEED_DIV64				5
